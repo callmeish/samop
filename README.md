e77d52bbc983694069b62109026a3731e8cfa5a5
Hextaine <hextaine@zoho.com>
2019-08-29 13:23:02 -0700

    Created README.md and established basic file structure for Spring.io server.

---
+++ README.md
@@ -0,0 +1,4 @@
+# S.A.M.O.P.
+The Simple Albion Market Order Processor, or SAMOP for short, is a data analysis and representation web application that
+utilizes the Albion Data Project (https://www.albion-online-data.com/) to provide "helpful" representations of market 
+orders, price histories, etc.
---
+++ backend/src/main/java/me/chrissch/projects/samop/Samop.java
@@ -0,0 +1,15 @@
+package me.chrissch.projects.samop;
+/*
+ * Created by Chris S. on 8/29/2019.
+ * Github: https://github.com/cschenkh
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Samop {
+    public static void main(String[] args) {
+        SpringApplication.run(Samop.class, args);
+    }
+}
